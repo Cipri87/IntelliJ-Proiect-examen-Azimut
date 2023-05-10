44e77ea6be4cbfeb6a4bcb1a5dc08492eb68a458
Ciprian Bogdan <bogdan.ciprian2587@yahoo.ro>
2023-05-10 13:46:36 +0300

    Commit chromedriver

--- src/main/java/stepsDefinition/Amazon.java
+++ src/main/java/stepsDefinition/Amazon.java
@@ -24,7 +24,7 @@ public class Amazon {
     @Then("Enter email address and Continue")
     public void enter_email_address_and_continue() throws InterruptedException {
         WebDriverInitialize.getDriver().findElement(By.xpath("//input[@type='email']")).sendKeys(
-                "cici@yahoo.ro");
+                "bogdan.ciprian2587@yahoo.ro");
         sleep(2000);
         WebDriverInitialize.getDriver().findElement(By.id("continue")).click();
         sleep(2000);
@@ -33,10 +33,10 @@ public class Amazon {
     @Then("Enter password and Sign In")
     public void enter_password_and_sign_in() throws InterruptedException {
         WebDriverInitialize.getDriver().findElement(By.xpath("//input[@type='password']"))
-                .sendKeys("look20023");
+                .sendKeys("fba2017");
         sleep(2000);
         WebDriverInitialize.getDriver().findElement(By.xpath("//input[@id='signInSubmit']")).click();
-        sleep(5000);
+        sleep(3000);
     }
 
     @Then("Click All button and select Electronics then Office Electronics")
@@ -99,7 +99,7 @@ public class Amazon {
     @Then("Add Price option $25 to $50")
     public void add_the_price_option_$25_to_$50() throws InterruptedException {
         Actions action = new Actions(WebDriverInitialize.getDriver());
-        action.scrollByAmount(0, 1200).perform();
+        action.scrollByAmount(0, 1500).perform();
         sleep(2000);
         WebDriverInitialize.getDriver().findElement(By.xpath("//a[@data-csa-c-element-id='filter" +
                 "-price-25-50']")).click();
@@ -109,10 +109,11 @@ public class Amazon {
     @Then("Add Average Customer Review of 4 stars and up")
     public void add_average_customer_review_of_4_stars_and_up() throws InterruptedException {
         Actions action = new Actions(WebDriverInitialize.getDriver());
-        action.scrollByAmount(0, 2000).perform();
+        action.scrollByAmount(0, 500).perform();
 
         sleep(3000);
-        WebDriverInitialize.getDriver().findElement(By.xpath("//a[@data-csa-c-element-id='filter-star-rating-4']")).click();
+        WebDriverInitialize.getDriver().findElement(By.xpath("//a[@data-csa-c-element-id='filter-star-" +
+                "rating-4']")).click();
         sleep(2000);
     }
 
@@ -121,7 +122,8 @@ public class Amazon {
         Actions action = new Actions(WebDriverInitialize.getDriver());
         action.scrollByAmount(0, 2000).perform();
         sleep(2000);
-        WebDriverInitialize.getDriver().findElement(By.xpath("//*[@class='a-pagination']//a[contains(text(),'Next')]")).click();
+        WebDriverInitialize.getDriver().findElement(By.xpath("//*[@class='a-pagination']//" +
+                "a[contains(text(),'Next')]")).click();
         sleep(3000);
     }
 }
